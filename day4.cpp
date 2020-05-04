3c0748165e4078e73c15bd9dab95ae050099d2e6
PankajKaushik78 <kaushikpankaj78@gmail.com>
2020-05-05 03:38:43 +0530

    May Challenge

---
+++ day4.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        string binary = "";
+        while(num > 0){
+            int rem = num%2;
+            binary =  to_string(rem) + binary ;
+            num /= 2;
+        }
+        long long int ans = 0;
+        for(int i=binary.size()-1; i>=0; i--){
+            if(binary[i] == '0'){
+                int power =binary.size() - 1 - i;
+                ans += pow(2, power);
+            }
+        }
+        return ans;
+    }
+};
