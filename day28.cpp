64d324041d0a12d2d71b415042caa09b299929ad
PankajKaushik78 <kaushikpankaj78@gmail.com>
2020-05-29 09:27:20 +0530

    May challenge

---
+++ day28.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        vector<int> ans(num+1);
+        ans[0] = 0;
+        
+        for(int i=1; i<=num; i++){
+            if(i%2){
+                ans[i] = ans[i/2]+1;
+            }else{
+                ans[i] = ans[i/2];
+            }
+        }
+        
+        return ans;
+    }
+};
